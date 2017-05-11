31e6fbde63fd2cfc685d5b72027a8f8ff260a252
puneetbhagat <pbhagat@MTVL147a050cf.local>
2017-05-10 19:58:04 -0700

    Added AlarmManager file

---
+++ ConnectedAlarmApp/AlarmManager.swift
@@ -0,0 +1,31 @@
+//
+//  AlarmManager.swift
+//  ConnectedAlarmApp
+//
+//  Created by Bhagat, Puneet on 5/7/17.
+//  Copyright © 2017 connected alarm app. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class AlarmManager: NSObject {
+    
+    static func PlayAlarm(view: UIViewController) {
+        
+        // create a sound ID, in this case its the tweet sound.
+        let systemSoundID: SystemSoundID = 1016
+        
+        // to play sound
+        let alarmTimer = Timer.scheduledTimer(withTimeInterval: 0.10, repeats: true, block: { (timer) in
+            AudioServicesPlaySystemSound (systemSoundID)
+        })
+        
+        let alert = UIAlertController(title: "Alarm", message: nil, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.cancel, handler: { (action) in
+            alarmTimer.invalidate()
+            alarmTimer = nil
+        }))
+        view.present(alert, animated: true, completion: nil)
+    }
+}
