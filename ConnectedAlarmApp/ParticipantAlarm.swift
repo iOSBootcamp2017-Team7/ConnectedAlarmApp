aa9d333d9166af414f67102a86a41ddf2261df5a
Uttam Singh <uttam_singh@intuit.com>
2017-05-16 09:21:52 +0530

    Model

--- ConnectedAlarmApp/ParticipantAlarm.swift
+++ ConnectedAlarmApp/ParticipantAlarm.swift
@@ -11,7 +11,7 @@ import Parse
 
 class ParticipantAlarm: BaseEntity {
     @NSManaged var user: PFUser!
-    @NSManaged var alarms: [Alarm]!
+    @NSManaged var alarms: Alarm!
     @NSManaged var status: String?
 }
 
